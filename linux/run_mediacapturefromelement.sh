61e5025f000fd6129bcf9f403c0e8856bf915656
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-03-03 15:56:04 -0800

    new codepen

--- linux/run_mediacapturefromelement.sh
+++ linux/run_mediacapturefromelement.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 CMD="./out/gn/chrome \
--vmodule='*html_*_element_capturer*=2,*media_stream*=1' \
+-vmodule='*html_*_element_capturer*=2,*HTMLMediaElementCapture*=1' \
 --use-fake-device-for-media-stream \
 --use-fake-ui-for-media-stream \
 --enable-blink-features=MediaCaptureFromVideo \
-https://rawgit.com/Miguelao/demos/master/videoelementcapture.html"
+https://codepen.io/miguelao/pen/OpXVVK?editors=1010"
+#https://rawgit.com/Miguelao/demos/master/videoelementcapture.html"
 
 echo $(tput setaf 2) $CMD $(tput sgr0)
 eval $CMD
