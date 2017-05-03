f7e8d926404ad50b39af103a4e7533e4b9e50df6
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-05-02 17:56:11 -0700

    updated two scripts

--- linux/run_imagecapture_fake.sh
+++ linux/run_imagecapture_fake.sh
@@ -4,8 +4,7 @@ CMD="./out/gn/chrome \
 -vmodule='*ImageCapture*=2,*image_capture*=1' \
 --use-fake-device-for-media-stream \
 --use-fake-ui-for-media-stream \
---enable-blink-features=ImageCapture \
-https://rawgit.com/Miguelao/demos/master/imagecapture.html"
+https://rawgit.com/yellowdoge/demos/master/imagecapture.html"
 
 echo $(tput setaf 2) $CMD $(tput sgr0)
 eval $CMD
